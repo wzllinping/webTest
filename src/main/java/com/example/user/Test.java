7f8d7ba3cc3cb3b24939763b34160d7c6e8b1782
wuyang <wuyang@tsign.cn>
2026-03-09 17:46:04 +0800

    test类修改

--- src/main/java/com/example/user/Test.java
+++ src/main/java/com/example/user/Test.java
@@ -8,6 +8,7 @@ public class Test {
             userInfo.setName("zhang");
             System.out.println(userInfo.getName());
             System.out.println(userInfo1.getName());
+            System.out.println("成功");
         } catch (Exception exception) {
 
         }
